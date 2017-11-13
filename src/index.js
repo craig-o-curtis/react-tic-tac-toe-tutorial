b530c07b167b050a9c1667cbe0ddb0129df76fbe
Craig Curtis <craigcurtis@Craigs-MacBook-Pro.local>
2017-11-12 20:12:06 -0700

    Avoid confusion of onClick, change to more angular-esque function names

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import RaisedButton from 'material-ui/RaisedButton';
 
 class Square extends React.Component {
+  /* state managed by parent Board component */
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
-        Hi
+      <button className="square" onClick={ () => this.props.toggleSquare() }>
+        {this.props.value}
       </button>
     );
   }
 }
 
+
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleToggle(i) {
+    const squares = this.state.squares.slice();
+    /* slice is to make a copy of the array */    
+    squares[i] = 'X';
+    this.setState( {squares: squares} )
+  }
+
+  /* used in JSX below {this.renderSquare(0)} */
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        toggleSquare={() => this.handleToggle(i)}
+      />
+    );
   }
 
   render() {
@@ -28,13 +51,13 @@ class Board extends React.Component {
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
+        {/* </div>
+        <div className="board-row"> */}
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
+        {/* </div>
+        <div className="board-row"> */}
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -47,15 +70,17 @@ class Board extends React.Component {
 class Game extends React.Component {
   render() {
     return (
-      <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
+      <MuiThemeProvider>
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
